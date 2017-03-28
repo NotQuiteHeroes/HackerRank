104067ff7bf7a173cb3cc4b54ccaae39e40c6ea1
Paige Eckstein <paige.eckstein@gmail.com>
2017-03-27 22:16:14 -0500

    Create String_Split_and_Join.py

---
+++ Python/Strings/String_Split_and_Join.py
@@ -0,0 +1,18 @@
+'''
+Task 
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+Input Format 
+The first line contains a string consisting of space separated words.
+https://www.hackerrank.com/challenges/python-string-split-and-join
+'''
+
+if __name__ == '__main__':
+    line = raw_input()
+    result = split_and_join(line)
+    print result
+    
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
