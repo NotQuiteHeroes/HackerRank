b01a40431ba97bad311ddacb476318ef7c5616ee
Paige Eckstein <paige.eckstein@gmail.com>
2017-03-27 21:15:07 -0500

    Create A_Personalized_Echo.sh

---
+++ LinuxShell/Bash/A_Personalized_Echo.sh
@@ -0,0 +1,5 @@
+# Write a Bash script which accepts name as input and displays a greeting: "Welcome (name)"
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo
+
+read name
+echo "Welcome $name"
