1e4c0e2c0d900776001c2e5dc35da256484bd7a9
Paige Eckstein <paige.eckstein@gmail.com>
2017-03-27 21:00:27 -0500

    Create Weather_Observation_Station_9.sql

---
+++ SQL/BasicSelect/Weather_Observation_Station_9.sql
@@ -0,0 +1,7 @@
+/* MySQL
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+https://www.hackerrank.com/challenges/weather-observation-station-9
+*/
+
+SELECT DISTINCT city FROM station WHERE city REGEXP "^[^aeiou].*";
