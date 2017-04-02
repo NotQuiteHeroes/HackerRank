8ab2a576fe53a31492df124a79fd3fe139cc7716
Paige Eckstein <paige.eckstein@gmail.com>
2017-04-02 14:22:19 -0500

    Update Triangle_Quest_2.py

--- Python/Math/Triangle_Quest_2.py
+++ Python/Math/Triangle_Quest_2.py
@@ -19,5 +19,7 @@ Print the palindromic triangle of size N as explained above.
 https://www.hackerrank.com/challenges/triangle-quest-2
 '''
 
+# (((10**i)-1)/9) is equation for repunit numbers: 1, 11, 111, 1111,...
+# Calculate square of each repunit to get Demlo numbers
 for i in range(1,int(raw_input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print(((10**i)-1)/9)**2
